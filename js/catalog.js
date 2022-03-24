1134069599134b7307465b6b94b5d7afb6b6e52b
raul <zarater.raul@gmail.com>
2022-03-24 12:42:40 -0700

    added init stuff with Audrey's input

--- js/catalog.js
+++ js/catalog.js
@@ -9,10 +9,13 @@ const cart = new Cart([]);
 // (the things in the Product.allProducts array) into the drop down list.
 function populateForm() {
 
-  //TODO: Add an <option> tag inside the form's select for each product
+  //DONE: Add an <option> tag inside the form's select for each product
   const selectElement = document.getElementById('items');
   for (let i in Product.allProducts) {
-
+    let optionElem = document.createElement('option');
+    optionElem.textContent = Product.allProducts[i].name;
+    optionElem.value = Product.allProducts[i].name;
+    selectElement.appendChild(optionElem);
   }
 
 }
@@ -22,7 +25,8 @@ function populateForm() {
 // so that it shows the # of items in the cart and a quick preview of the cart itself.
 function handleSubmit(event) {
 
-  // TODO: Prevent the page from reloading
+  // DONE: Prevent the page from reloading
+  event.preventDefault();
 
   // Do all the things ...
   addSelectedItemToCart();
@@ -34,7 +38,9 @@ function handleSubmit(event) {
 
 // TODO: Add the selected item and quantity to the cart
 function addSelectedItemToCart() {
-  // TODO: suss out the item picked from the select list
+  // DONE: suss out the item picked from the select list
+  let item = document.getElementById('items').value
+  console.log(item);
   // TODO: get the quantity
   // TODO: using those, add one item to the Cart
 }
